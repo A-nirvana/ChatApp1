5e8e6746238a8a5f9bc3c4091991d1bcc9267ce1
A-nirvana <anirban_ug_23@gmail.com>
2024-05-28 21:05:21 +0530

    Improved the previous UI

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { useEffect, useState } from 'react'
 import Contacts from '../contacts/page'
+import "./chat.css"
+import ReactTextareaAutosize from 'react-textarea-autosize';
 
 function App() {
   // const [socket, setSocket] = useState<WebSocket | null>(null);
@@ -22,18 +24,26 @@ function App() {
     last: string;
   }
 
-  const [current, setCurrent] = useState<Chat>({name: "1", last: "Abrea"});
+  const [current, setCurrent] = useState<Chat>({name: "", last: ""});
 
   return (
     <main className='min-h-screen w-screen flex'>
-      <section className=' bg-white dark: bg-slate-900 w-1/4'>
-        <div className=' h-14 shadow-md shadow-slate-950 -mb-1 mb-4 pr-1'>ew</div>
+      <section className=' bg-slate-900 dark: bg-slate-900 w-1/4'>
+        <div className=' h-14 shadow-md shadow-slate-950 -mb-1 mb-4 pr-1 text-center text-xl font-semibold font-mono pt-3 flex justify-center'>
+          <img src='../favicon.ico' className='h-6'/>HuntersOfficial</div>
         <div>{Contacts(setCurrent)}</div>
         
       </section>
       <section className='bg-white dark:bg-slate-800 w-3/4 text-slate-950 dark:text-white'>
-        <div className=' h-14 shadow-md shadow-slate-950 mb-4'>{current.name}</div>
-        <div >{current.last}</div>
+        <div className=' h-14 shadow-md shadow-slate-950 mb-4 text-center'><p>{current.name}</p>{current.last}</div>
+        {current.name &&
+        <div>
+        <ReactTextareaAutosize maxRows={6} className=' absolute bottom-8 w-1/2 left-1/3 bg-slate-700 bg-opacity-70 rounded-md py-2 px-6 shadow-xl'/>
+        </div>
+        }
+        <div>
+
+        </div>
       </section>
       
     </main> 
--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -5,34 +5,42 @@ import { useState } from "react";
 
 export default function Contacts(set : Function) 
 {
-    const [current, setCurrent] = useState<Object | null>(null);
+    interface Chat{
+        name: string;
+        last: string;
+    }   
+    const [current, setCurrent] = useState<Chat>({name: "", last: ""});
     const arr = [
-        { name: "1", last: "Abrea" },
-        { name: "2", last: "Babdjba" },
-        { name: "3", last: "Coifdi" },
-        { name: "4", last: "Doihdsaio" },
-        { name: "5", last: "Eiew" },
-        { name: "6", last: "Fjeij" },
-        { name: "7", last: "Goihdias" },
-        { name: "8", last: "Hisioa" },
-        { name: "9", last: "Ijsdao" },
-        { name: "10", last: "Jaisd" }
+        { name: "John Doe", last: "Abrea" },
+        { name: "Jane Smith", last: "Babdjba" },
+        { name: "Michael Johnson", last: "Coifdi" },
+        { name: "Emily Davis", last: "Doihdsaio" },
+        { name: "Daniel Wilson", last: "Eiew" },
+        { name: "Olivia Brown", last: "Fjeij" },
+        { name: "Sophia Taylor", last: "Goihdias" },
+        { name: "Matthew Anderson", last: "Hisioa" },
+        { name: "Emma Thomas", last: "Ijsdao" },
+        { name: "William Martinez", last: "Jaisd" }
     ];
+
     return (
-        <main className="flex h-auto flex-col items-center justify-between space-y-2">
-            {arr.map((item) =>{
+        <main className="flex h-auto flex-col items-center justify-between">
+            {arr.map((item) => {
                 return (
-                    <div key={item.name} className={`border-b-2 border-slate-900 w-full h-12 cursor-pointer px-2 ${current == item ? "bg-slate-600" : "bg-slate-900"}`}
-                        onClick={()=>{
+                    <div
+                        key={item.name}
+                        className={`border-b-2 border-slate-950 w-full h-12 cursor-pointer px-2 ${current.name === item.name && "bg-slate-800" }`}
+                        onClick={() => {
                             setCurrent(item);
                             set(item);
+                            console.log(current);
                         }}
                     >
-                        <p className=" text-md text-black dark:text-white">{item.name}</p>
-                        <p className=" text-xs text-gray-600">{item.last}</p>
+                        <p className="text-md text-black dark:text-white">{item.name}</p>
+                        <p className="text-xs text-gray-600">{item.last}</p>
                     </div>
-                )
-            } )}
+                );
+            })}
         </main>
-    )       
+    );
 }
